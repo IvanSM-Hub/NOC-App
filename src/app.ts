a0cb7fee8a72531a9b241bfc4fc9e6e2d6ff47b4
Iván Sarió Madrigal <ivan.sario.madrigal@gmail.com>
2024-09-13 10:57:37 +0200

    add NOC-APP basic structure

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import { ServerApp } from "./presentation/server";
+
+
+( async() => {
+    main()
+} ) ();
+
+function main() {
+    ServerApp.start();
+}
+
---
+++ src/presentation/server.ts
@@ -0,0 +1,26 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CronService } from "./cron/cron-service";
+
+export class ServerApp {
+
+    public static start() {
+
+        console.log('Server started...');
+
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                const url = 'http://google.es';
+                new CheckService(
+                   () => console.log(`${url} is ok`),
+                   ( error ) => console.log( error ),
+                ).execute( url );
+                // new CheckService().execute('http://localhost:3000');
+            }
+            
+        );
+        
+
+    }
+
+}
